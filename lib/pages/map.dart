a9dfe9e5ad57e03f8e79e8eabd42a413c83a69d0
Kryštof Krátký <krystof.kratky2003@gmail.com>
2022-02-03 06:27:35 +0100

    map

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Home Tab'),
+      child: FlutterMap(
+      options: MapOptions(
+      center: latLng.LatLng(51.5, -0.09),
+      zoom: 13.0,
+    ),
+    layers: [
+      TileLayerOptions(
+        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        subdomains: ['a', 'b', 'c'],
+        attributionBuilder: (_) {
+          return Text("© OpenStreetMap contributors");
+        },
+      ),
+      MarkerLayerOptions(
+        markers: [
+          Marker(
+            width: 80.0,
+            height: 80.0,
+            point: latLng.LatLng(58.8, 6.1),
+            builder: (ctx) =>
+            Container(
+              child: const Icon(CupertinoIcons.map_pin, color: CupertinoColors.white,),
+            ),
+          ),
+        ],
+      ),
+    ],
+  )
     );
   }
 }
